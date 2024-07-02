378832186edfc4ea368fe5fa62b459ac626fadb0
Gil Tinde <gil.tinde@pm.me>
2024-07-02 10:01:14 +0200

    fix: spacing

--- tests/test_crop_health_client.py
+++ tests/test_crop_health_client.py
@@ -42,6 +42,7 @@ class TestCropHealthClient:
         with open(self.image_path, "rb") as f:
             image_data = f.read()
             health = await AsyncCropHealthClient.get_binary_prediction(image_data)
+
         # Assert that the sum of all values is approx equal to 1.0
         assert sum(v for _, v in health if v is not None) == pytest.approx(
             1.0, rel=1e-1
@@ -52,6 +53,7 @@ class TestCropHealthClient:
         with open(self.image_path, "rb") as f:
             image_data = f.read()
             health = await AsyncCropHealthClient.get_singleHLT_prediction(image_data)
+
         # Assert that the sum of all values is approx equal to 1.0
         assert sum(v for _, v in health if v is not None) == pytest.approx(
             1.0, rel=1e-1
@@ -62,6 +64,7 @@ class TestCropHealthClient:
         with open(self.image_path, "rb") as f:
             image_data = f.read()
             health = await AsyncCropHealthClient.get_multiHLT_prediction(image_data)
+            
         # Assert that the sum of all values is approx equal to 1.0
         assert sum(v for _, v in health if v is not None) == pytest.approx(
             1.0, rel=1e-1
