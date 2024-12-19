890bb62d44b356ea1fb86405268e0767109fe99e
daniejoh <daniel.johnsen@knowit.no>
2024-12-19 10:18:52 +0100

    feat: add agriculture

---
+++ openepi_client/agriculture/__init__.py
@@ -0,0 +1,11 @@
+from ._agriculture_types import (
+    Deforestation,
+    IntensityEnum,
+    PeakTimingEnum,
+    SoilType,
+    Weather,
+    Flood,
+    Summary,
+)
+
+from ._agriculture_client import AgricultureClient, AsyncAgricultureClient
