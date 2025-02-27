c65a51a1c375321d250d2b4b98f5132de40b57ca
daniejoh <daniel.johnsen@knowit.no>
2025-02-27 12:06:26 +0100

    fix: format code

--- tests/test_agriculture_client.py
+++ tests/test_agriculture_client.py
@@ -2,7 +2,12 @@ import pytest
 from unittest.mock import patch, AsyncMock
 
 
-from openepi_client.agriculture import AgricultureClient, AsyncAgricultureClient, PeakTimingEnum, IntensityEnum
+from openepi_client.agriculture import (
+    AgricultureClient,
+    AsyncAgricultureClient,
+    PeakTimingEnum,
+    IntensityEnum,
+)
 from openepi_client import GeoLocation
 from openepi_client.agriculture._agriculture_types import Summary
 
@@ -23,9 +28,11 @@ class TestAgricultureClient:
         assert summary.weather.air_temperature == 123.456
         assert summary.flood.peak_step == 10
 
-
     @pytest.mark.asyncio
-    @patch("openepi_client.agriculture.AsyncAgricultureClient.get_summary", new_callable=AsyncMock)
+    @patch(
+        "openepi_client.agriculture.AsyncAgricultureClient.get_summary",
+        new_callable=AsyncMock,
+    )
     async def test_async_get_summary(self, mock_get_summary):
         mock_get_summary.return_value = MockSummary()
         summary: Summary = await AsyncAgricultureClient.get_summary(
@@ -37,12 +44,13 @@ class TestAgricultureClient:
         assert summary.flood.peak_step == 10
 
 
-
 class MockSummary:
     class Deforestation:
         daterange_tot_treeloss = 123.456
+
     class SoilType:
         most_probable_soil_type = "Somesoil"
+
     class Weather:
         air_temperature = 123.456
         cloud_area_fraction = 123.456
@@ -52,17 +60,16 @@ class MockSummary:
         wind_from_direction = 123.456
         wind_speed = 123.456
         wind_speed_of_gust = 123.456
+
     class Flood:
         error = "Some error"
         intensity = "P"
         issued_on = "some date"
-        peak_day =  "some date"
+        peak_day = "some date"
         peak_step = 10
         peak_timing = "BB"
-    
+
     deforestation = Deforestation()
     soil = SoilType()
     weather = Weather()
     flood = Flood()
-
-    
